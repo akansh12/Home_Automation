9093e2c35e5f9cc6834dbcb7e044a3c2b4cf535c
akansh12 <39628860+akansh12@users.noreply.github.com>
2019-03-30 23:25:28 +0530

    Add files via upload

---
+++ Home_Automation_Techkriti.ino
@@ -0,0 +1,218 @@
+
+  
+ ****************************************************/
+//#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include "DHT.h"
+
+
+
+
+
+
+/************************* Pin Definition *********************************/
+
+//Relays for switching appliances
+int Relay1=D1;
+int Relay2=D2;
+int Relay3=D3;
+int Relay4=D4;
+
+//DHT11 for reading temperature and humidity value
+#define DHTPIN            D7
+
+
+//Analog pin to read the incoming analog value from Light sensors.
+int motion = D5;
+int ldr=A0;
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "MI"
+#define WLAN_PASS       "12345678"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "Akansh"
+#define AIO_KEY         "c2aed33b697a41e5aa5931ab211021de"
+
+/************ Global State ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish Humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
+Adafruit_MQTT_Publish Temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+Adafruit_MQTT_Publish Motion = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/pir-sensor");
+Adafruit_MQTT_Publish LDR light value = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/ldr-sensor");
+Adafruit_MQTT_Publish Soil_Moisture = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/soil-moisture");
+
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay-1");
+Adafruit_MQTT_Subscribe Light2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay-2");
+Adafruit_MQTT_Subscribe Light3 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay-3");
+Adafruit_MQTT_Subscribe Light4 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay-4");
+
+
+
+/************ Necessary declaration for DHT11 ******************/
+#define DHTTYPE           DHT11     
+
+DHT dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+
+/*************************** Sketch Code ************************************/
+void MQTT_connect();
+Servo myservo;
+void setup() {
+  Serial.begin(115200);
+
+  delay(10);
+
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  pinMode(Relay3, OUTPUT);
+  pinMode(Relay4, OUTPUT);
+  pinMode(ldr, INPUT);
+  pinMode(motion, INPUT);
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  
+  //Setting up DHT sensor
+  dht.begin();
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&Light1);
+  mqtt.subscribe(&Light2);
+  mqtt.subscribe(&Light3);
+  mqtt.subscribe(&Light4);
+}
+
+uint32_t x = 0;
+
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+  // this is our 'wait for incoming subscription packets' busy subloop
+  // try to spend your time here
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(20000))) {
+    if (subscription == &Light1) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light1.lastread);
+      int Light1_State = atoi((char *)Light1.lastread);
+      digitalWrite(Relay1, Light1_State);
+      
+    }
+    if (subscription == &Light2) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light2.lastread);
+      int Light2_State = atoi((char *)Light2.lastread);
+      digitalWrite(Relay2, Light2_State);
+    }
+    if (subscription == &Light3) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light3.lastread);
+      int Light3_State = atoi((char *)Light3.lastread);
+      digitalWrite(Relay3, Light3_State);
+    }
+    if (subscription == &Light4) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)Light4.lastread);
+      int Light4_State = atoi((char *)Light4.lastread);
+      digitalWrite(Relay4, Light4_State);
+      
+    }
+  }
+
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  if (! Humidity.publish(h)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+  if (! Temperature.publish(t)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+  
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  /*
+    if(! mqtt.ping()) {
+    mqtt.disconnect();
+    }
+  */
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+ 
+}
---
+++ Ultimate_home_Automation.ino
@@ -0,0 +1,74 @@
+//Declaring libraris
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
+
+
+int bulb1=11;
+int bulb2=12;
+int bulb3=13;                
+int pir_sensor = 10; 
+int ldr = A0;             
+int state = LOW;             
+int val = 0;
+void setup() {
+  lcd.begin(16,2);
+  pinMode(pir_sensor, INPUT);
+  pinMode(ldr,INPUT);
+  Serial.begin(9600);  
+  pinMode(bulb3, OUTPUT);
+  pinMode(bulb2,OUTPUT);
+
+  //Initial Display
+  lcd.setCursor(0,0);
+  lcd.print("Home Automation");
+  delay(2000);
+  lcd.clear();
+  lcd.print("OUT1  OUT3  OUT4");
+  lcd.setCursor(0,1);
+  lcd.print("OFF    OFF   OFF");
+
+}
+
+void loop() {
+
+  lcd.setCursor(0,0);
+  lcd.print("OUT1  OUT3  OUT4");
+ 
+  // PIR_Sensor_operated_switch
+  
+  val = digitalRead(pir_sensor);   
+  if (val == HIGH) {           
+    digitalWrite(bulb3, HIGH);
+     lcd.setCursor(7,1); 
+      lcd.print("ON    ");
+   //LDR_sensor
+  int val = analogRead(ldr);
+  analogWrite(bulb2, val / 10 );
+  Serial.println(val);
+    delay(5000);                 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       
+    }
+    delay(5000);
+  }
+
+   if (val==LOW){
+    digitalWrite(bulb3, LOW);
+     lcd.setCursor(7,1); 
+     lcd.print("OFF   ");
+  //LDR_sensor
+  int val = analogRead(ldr);
+  analogWrite(bulb2, val / 10 );
+  Serial.println(val);
+    
+   }
+
+  
+ 
+
+  
+  
+
+}
